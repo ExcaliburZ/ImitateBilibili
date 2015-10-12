d9bf10f98e1de73e79374e335c659be15b86ca69
ExcaliburZ <419472353@qq.com>
2015-10-12 22:55:03 +0800

    make  a splash activity

--- zilizili/src/main/java/com/wings/zilizili/SplashActivity.java
+++ zilizili/src/main/java/com/wings/zilizili/SplashActivity.java
@@ -9,5 +9,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        System.out.println("");
     }
 }
