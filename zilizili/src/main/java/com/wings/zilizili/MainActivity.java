6e6e108c4d1e79d6cbe6211bb573312cf83db026
ExcaliburZ <419472353@qq.com>
2015-10-19 17:10:04 +0800

    move left menu to fragment and complete surface

--- zilizili/src/main/java/com/wings/zilizili/MainActivity.java
+++ zilizili/src/main/java/com/wings/zilizili/MainActivity.java
@@ -1,15 +1,18 @@
 package com.wings.zilizili;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.wings.zilizili.fragment.LeftMenuFragment;
+
+public class MainActivity extends AppCompatActivity implements LeftMenuFragment.OnFragmentInteractionListener {
 
     private Toolbar toolbar;
 
@@ -51,5 +54,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
