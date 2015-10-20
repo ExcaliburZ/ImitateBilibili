e41514ec7109d28eecbc2aedb9343f3a45fdbc50
ExcaliburZ <419472353@qq.com>
2015-10-20 19:11:53 +0800

    Refactoring left menu and new tabIndicator.

--- zilizili/src/main/java/com/wings/zilizili/MainActivity.java
+++ zilizili/src/main/java/com/wings/zilizili/MainActivity.java
@@ -4,6 +4,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,20 +16,56 @@ import android.view.View;
 
 import com.wings.zilizili.fragment.LeftMenuFragment;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements LeftMenuFragment.OnFragmentInteractionListener {
 
-    private Toolbar toolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private FloatingActionButton mFab;
+    private TabLayout mTabLayout;
+    private ViewPager mMainContent;
+    private ArrayList<String> mTabLists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        init();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    private void findView() {
+        mToolbar = $(R.id.toolbar);
+        mDrawerLayout = $(R.id.dl_main);
+        mFab = $(R.id.fab);
+        mTabLayout = $(R.id.tab_layout);
+        mMainContent = $(R.id.vp_main);
+        mTabLists = new ArrayList<String>() {{
+            add("番剧");
+            add("推荐");
+            add("分区");
+            add("关注");
+            add("发现");
+        }};
+    }
+
+    private void init() {
+        findView();
+        setToggleLeftDrawer();
+        mToolbar.setTitle(R.string.home_top_default_nickname);
+        initViewPager();
+        setListener();
+    }
+
+    private void initViewPager() {
+        for (String tab : mTabLists) {
+            mTabLayout.addTab(mTabLayout.newTab().setText(tab));
+        }
+    }
+
+    private void setListener() {
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -33,6 +73,16 @@ public class MainActivity extends AppCompatActivity implements LeftMenuFragment.
             }
         });
     }
+
+    private void setToggleLeftDrawer() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,5 +109,9 @@ public class MainActivity extends AppCompatActivity implements LeftMenuFragment.
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    private <T extends View> T $(int resId) {
+        return (T) super.findViewById(resId);
     }
 }
--- zilizili/src/main/java/com/wings/zilizili/fragment/LeftMenuFragment.java
+++ zilizili/src/main/java/com/wings/zilizili/fragment/LeftMenuFragment.java
@@ -72,7 +72,7 @@ public class LeftMenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_left_menu, container, false);
+        return inflater.inflate(R.layout.navigation_layout, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
